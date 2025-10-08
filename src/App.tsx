6745cb86d5f26f2eaf33f196a4a3b7ae5c6f8da8
Aditya <workwith.adityapatil@gmail.com>
2025-10-09 00:16:23 +0530

    Fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-=======
-// import ChapterIndex from "./pages/dsa/ChapterIndex";
->>>>>>> 569dfba04fefcde837291b8a498917015e3ed7df
 import ChapterDetail from "./pages/dsa/ChapterDetail";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
@@ -19,8 +15,8 @@ import DevelopmentPage from "./pages/development/DevelopmentPage";
 import AptitudePage from "./pages/aptitude/AptitudePage";
 import AptitudeChapterDetail from "./pages/aptitude/AptitudeChapterDetail";
 import DevelopmentChapterDetail from "./pages/development/DevelopmentChapterDetail";
-import PreparePage from "./pages/prepare/PreparePage";
-import PrepareChapterDetail from "./pages/prepare/PrepareChapterDetail";
+// import PreparePage from "./pages/prepare/PreparePage";
+// import PrepareChapterDetail from "./pages/prepare/PrepareChapterDetail";
 
 const queryClient = new QueryClient();
 
@@ -53,11 +49,11 @@ const App = () => (
                 <Route index element={<div className="text-center text-2xl font-bold">Welcome to DSA! Select a chapter from the sidebar.</div>} />
               </Route>
 
-              <Route path="/prepare" element={<PreparePage />}>
+              {/* <Route path="/prepare" element={<PreparePage />}>
                 <Route path=":chapterId/:subChapterId" element={<PrepareChapterDetail />} />
                 <Route path="quiz/:chapterId" element={<PrepareChapterDetail />} />
                 <Route index element={<div className="text-center text-2xl font-bold">welcome to Interview preparation for prepare Interview</div>} />
-              </Route>
+              </Route> */}
               
             </Route>
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
