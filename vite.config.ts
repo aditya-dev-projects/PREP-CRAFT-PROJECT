1e97c091ff882b8230f8fb13183823fb9fcf6430
Aditya <workwith.adityapatil@gmail.com>
2025-10-08 22:59:08 +0530

    Add automatic deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { visualizer } from "rollup-plugin-visualizer"; // <--- 1. ADD THIS LINE
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
-export default defineConfig({ // Removed ({ mode }) => for simplicity
+export default defineConfig({
+  base: './', // <-- ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    visualizer({ // <--- 2. ADD THIS PLUGIN CALL
+    visualizer({
       filename: 'bundle-stats.html',
       open: true,
     }),
